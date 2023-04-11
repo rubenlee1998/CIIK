6a0335d6d449d90bb8031caadb8b22b52b24862a
RubenBoard <klsj9810@gmail.com>
2023-04-11 22:14:29 +0900

    fourth

--- javascripts/mainPage.js
+++ javascripts/mainPage.js
@@ -1,1 +1,22 @@
-const loadText = document.getElementById("first-text-load");
+// JavaScript 코드
+const title = document.getElementById("title");
+
+const subtitle = document.getElementById("subtitle");
+
+title.addEventListener("mouseover", function () {
+  setTimeout(function () {
+    title.classList.remove("animated-text");
+  }, 1600);
+  title.classList.add("hidden");
+  subtitle.classList.remove("hidden");
+  subtitle.classList.add("animated-text");
+});
+
+subtitle.addEventListener("mouseleave", function () {
+  setTimeout(function () {
+    subtitle.classList.remove("animated-text");
+  }, 1600);
+  subtitle.classList.add("hidden");
+  title.classList.remove("hidden");
+  title.classList.add("animated-text");
+});
